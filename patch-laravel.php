a2b5fd8c2b6e0e7ce2cd68cffc78b95b549b65a9
batuhan196 <batuhan196@users.noreply.github.com>
2026-06-08 12:09:10 +0300

    add laravel framework patch to workflow

---
+++ patch-laravel.php
@@ -0,0 +1,27 @@
+<?php
+$file = __DIR__ . '/vendor/laravel/framework/src/Illuminate/Support/Str.php';
+if (file_exists($file)) {
+    $content = file_get_contents($file);
+    
+    // Replace double-quoted default characters with single-quoted ones to prevent raw null bytes
+    $content = str_replace(
+        '$trimDefaultCharacters = " \n\r\t\v\0";',
+        '$trimDefaultCharacters = \' \n\r\t\v\0\';',
+        $content
+    );
+    $content = str_replace(
+        '$ltrimDefaultCharacters = " \n\r\t\v\0";',
+        '$ltrimDefaultCharacters = \' \n\r\t\v\0\';',
+        $content
+    );
+    $content = str_replace(
+        '$rtrimDefaultCharacters = " \n\r\t\v\0";',
+        '$rtrimDefaultCharacters = \' \n\r\t\v\0\';',
+        $content
+    );
+
+    file_put_contents($file, $content);
+    echo "Laravel Framework patched successfully!\n";
+} else {
+    echo "Error: Str.php not found!\n";
+}
